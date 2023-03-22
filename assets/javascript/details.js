1dcedab521b5880054a4f6f3db45e04fb6379249
cositofragil <magy_1398@hotmail.com>
2023-03-21 21:30:21 -0300

    arreglos

--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -28,21 +28,24 @@ function cardsDetails(evento) {
             <div class="col-xs-12 col-sm-12 col-lg-5 bg-success-subtle p-2 text-dark">
                 <img src="${evento.image}" class="card-img-detail" alt="...">
             </div>
-            <div class="col-xs-12 col-sm-12 col-lg-5 bg-success-subtle p-3 text-dark flex-column align-items-center justify-content-between">
-                <h5 class="card-title">${evento.name}</h5>
+            <div class="col-xs-12 col-sm-12 col-lg-5 bg-success-subtle p-3 text-dark text-center">
+                <h2 class="card-title mb-5">${evento.name}</h2>
                 <div class="mt-3 mb-2">
-                    <span class="card-date m-1"><i class="fa fa-calendar" aria-hidden="true"></i>${evento.date}</span>
-                    <span class="card-price m-1"><i class="fa fa-usd" aria-hidden="true"></i>${evento.price}</span>
+                    <span class="card-date m-1"><i class="fa fa-calendar" aria-hidden="true"></i><b>DATE:</b> ${evento.date}</span>
+                    <span class="card-price m-1"><b>PRICE:</b> <i class="fa fa-usd" aria-hidden="true"></i>${evento.price}</span>
                 </div>
                 <div class="mt-3 mb-2">
-                    <span class="card-place m-1"><i class="bi bi-geo-alt" aria-hidden="true"></i>${evento.place}</span>
-                    <span class="card-assistance m-1"><i class="bi bi-people-fill" aria-hidden="true"></i>${evento.assistance}</span>
+                    <span class="card-place m-1"><i class="bi bi-geo-alt" aria-hidden="true"></i><b>PLACE:</b> ${evento.place}</span>
+                    <span class="card-assistance m-1"><i class="bi bi-people-fill" aria-hidden="true"></i><b>ASSISTANCE:</b> ${evento.assistance}</span>
                 </div>
                 <div class="mt-3 mb-5">
-                    <span class="card-category">${evento.category}</span>
-                    <span class="card-capacity"><i class="fa fa-users" aria-hidden="true"></i>${evento.capacity}</span>
+                    <span class="card-category"><b>CATEGORY:</b> ${evento.category}</span>
+                    <span class="card-capacity"><i class="fa fa-users" aria-hidden="true"></i><b>CAPACITY:</b> ${evento.capacity}</span>
                 </div>
-                <p class="card-text mt-3">${evento.description}</p>
+                <p class="card-text mt-3"><b>DESCRIPTION:</b> ${evento.description}</p>
+                <div class="divVolver mt-3 mb-1 col-12 w-50">
+                    <a href="/index.html" class="btn btn-success d-block">Volver al inicio</a>
+                </div>
             </div>`
     } catch (error){
         console.log(error);
